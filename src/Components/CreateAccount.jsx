194c0bbe3752eb3552b186efb8960d964c0228cb
Ravi Pal <77372752+Ravi148369@users.noreply.github.com>
2022-06-18 21:12:01 +0530

    save changes

--- src/Components/CreateAccount.jsx
+++ src/Components/CreateAccount.jsx
@@ -1,7 +1,7 @@
 import React,{useState} from 'react'
 import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
-
+import './index.css'
 const CreateAccount = () => {
   const Navigate=useNavigate()
   const [firstname,setFirstName]=useState('')
@@ -28,17 +28,19 @@ const CreateAccount = () => {
     <div className='account'>
       <div className="center">
         <h1>Create your Account</h1>
-        <div className='form'>
+        <div className='signupform'>
           <div className='name'>
             <input type="text" placeholder='First name' onChange={(e)=>setFirstName(e.target.value)}/>
             <input type="text" placeholder='Last name' onChange={(e)=>setLastName(e.target.value)}/>
           </div>
             <input type="text" placeholder='Username' id='uname' onChange={(e)=>setUsername(e.target.value)}/>
-            <div className='name'>
+          <div className='name'>
             <input type="password" placeholder='Password' />
             <input type="password" placeholder='Confirm' onChange={(e)=>setPassword(e.target.value)}/>
-            </div>
-            <input type="submit" value={"Create Account"} onClick={onSubmit}/> 
+          </div>
+          <div className='signup'>
+            <button onClick={onSubmit}>Create Account</button>
+          </div>
         </div>
       </div>
     </div>
